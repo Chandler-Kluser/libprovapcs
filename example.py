aa9b3c630513e1e8879e03a39d4a75bc16c6cafa
Chandler Klüser <xchandlerkc@hotmail.com>
2020-06-16 04:52:37 -0300

    Added entropy for decision trees calculations

--- example.py
+++ example.py
@@ -1,12 +1,21 @@
 import libprova as lb
 import numpy as np
 
+# cálculo de saída de perceptron com relu
+
 x = np.array([-3,10,-2])
 w = np.array([1,2,4])
 
 print(lb.relu(lb.wsum(w,x)))
 
-point1 = np.array([0,0])
-point2 = np.array([1,1])
+# cálculo de distância euclidiana
 
-print(lb.kmeans.calc_distance(point1,point2))
+a = np.array([0,0])
+b = np.array([0,2.4])
+print(lb.kmeans.calc_distance(a,b))
+
+# cálculo de entropia
+
+samples = np.array([7,2,4])
+
+print(lb.overall_entropy(samples))
--- libprova/__init__.py
+++ libprova/__init__.py
@@ -27,3 +27,26 @@ def relu(x):
         return x
     else:
         return 0
+
+def entropy(si,s):
+    """
+    Calculates the single entropy of a parameter
+    si: samples of a "i" out
+    s: all the samples count
+    """
+    if si==0:
+        return 0
+    else:
+        return -(si/s)*np.log2(si/s)
+
+def overall_entropy(si_s):
+    """
+    Calculates the overall entropy
+    si_s: np.array with all the samples count
+    """
+    a = si_s.shape[0]
+    sum = 0
+    s = np.sum(si_s)
+    for i in range(0,a):
+        sum += entropy(si_s[i],s)
+    return sum
